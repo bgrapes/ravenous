5edf3b7d066ffb993d69b9a5d29773691bfbb497
Brian Grapes <bgrapes@gmail.com>
2017-11-13 19:53:12 -0800

    Fix renderSortByOptions method in SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,12 +9,10 @@ const sortByOptions = {
 
 class SearchBar extends React.Component {
   renderSortByOptions() {
-    return (
-      Object.keys(sortByOptions).map(sortByOption => {
-        let sortByOptionValue = sortByOptions[sortByOption];
-        return <li key={sortByOptionValue}>sortByOption</li>;
-      })
-    )
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    })
   }
 
   render() {
